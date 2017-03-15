aeb1742049e0b944f0df622a33ebba330273f4d4
vivek <vivek.doraswamy@gmail.com>
2017-03-15 12:56:46 -0400

    first commit

---
+++ sample1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#Class C:
+#	@classmethod
+#	def f(arg1, arg2):
+#	print cmp (2,3)
+#	print complex (1,2)
+#	print  divmod(3,2)
+
+#print abs(-1)
+
+#a = [1,2,3,4,5]
+#print all(a)
+
+print bin(12)
+
+print cmp(2,3)
+print complex(1,2)
+print  divmod(3,2)
---
+++ sample2.py
@@ -0,0 +1,1 @@
+execfile('./sample1.py')
